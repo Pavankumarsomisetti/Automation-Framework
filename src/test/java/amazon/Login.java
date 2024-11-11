28d3f85ae0c152c8b7de536fc64a893bdb52ddc6
pavan <pavankumarsomisetti@gmail.com>
2024-11-11 10:55:20 +0530

    this updated by second eng

--- src/test/java/amazon/Login.java
+++ src/test/java/amazon/Login.java
@@ -8,7 +8,13 @@ public class Login extends Amazon_Test_scripts {
 	public void click_on_product() {
 		
      //driver.findElement(By.xpath("//img[@alt='Apple iPhone 15 (128 GB) - Black']")).click();
-       driver.findElement(By.xpath("(//div[contains(@class,'a-section a-spacing-none _fluid-fat-im')])[3]")).click();
+      // driver.findElement(By.xpath("(//div[contains(@class,'a-section a-spacing-none _fluid-fat-im')])[3]")).click();
+		System.out.println("product is clicked ");
+	}
+	
+	public void addtocart()
+	{
+		System.out.println("product is added to cart");
 	}
 
 }
